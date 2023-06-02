2f0cdd87cd8ee1dae69f9c51706053dab35a7f82
ARayane24 <at.rayane03@gmail.com>
2023-06-02 04:23:29 +0100

    linking

---
+++ Mini projet/postGet.php
@@ -0,0 +1,16 @@
+<?php
+// (B) PROCESS SEARCH WHEN FORM SUBMITTED
+if (isset($_POST["search"])) {
+  // (B1) SEARCH FOR USERS
+  require "3-search.php";
+
+  // (B2) DISPLAY RESULTS
+     if (count($results) > 0) {
+         foreach ($results as $r) {
+            printf("<div>%s - %s</div>", $r["name"], $r["email"]);
+        }
+    } else {  
+        echo "No results found";
+     }
+}
+?>
