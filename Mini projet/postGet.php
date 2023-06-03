8559e5658b0828487e27ec1c2127d112a43da001
ARayane24 <at.rayane03@gmail.com>
2023-06-03 19:19:38 +0100

    new page (ville details) / modification css, html

--- Mini projet/postGet.php
+++ Mini projet/postGet.php
@@ -1,16 +1,44 @@
-<?php
-// (B) PROCESS SEARCH WHEN FORM SUBMITTED
-if (isset($_POST["search"])) {
-  // (B1) SEARCH FOR USERS
-  require "3-search.php";
 
-  // (B2) DISPLAY RESULTS
-     if (count($results) > 0) {
-         foreach ($results as $r) {
-            printf("<div>%s - %s</div>", $r["name"], $r["email"]);
+<?php
+      $connect = new mysqli('localhost' , 'root' , '' , 'voyage');
+      if ( !($connect) ) {
+          die('Connection Failed');
+      }else{
+        $Continent = $_POST['Continent'];
+        $Ville = $_POST['Ville'];
+        $Pays = $_POST['Pays'];
+        $Site = $_POST['Site'];
+
+        $query = "SELECT * FROM cities WHERE continent='$continent' AND country='$country' AND city='$city' AND site='$site'";
+
+        $result = mysqli_query($conn, $query);
+
+        if ($result) {
+  
+            while ($row = mysqli_fetch_assoc($result)) {
+                // Access the row data
+                $cityName = $row['city'];
+                $countryName = $row['country'];
+
+                // Display the data
+                echo "<p>$cityName ($countryName)</p>";
+            }
+
+            // Free the result set
+            mysqli_free_result($result);
+        } else {
+              // Handle the case when the query fails
+            echo "Error: " . mysqli_error($conn);
         }
-    } else {  
-        echo "No results found";
-     }
-}
+
+        // Close the database connection
+        mysqli_close($conn);
+
+
+  
+      }
+
+    
+
+    
 ?>
