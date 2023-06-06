f1adca0d36ce2438d75b4ddd12e3c5f736a2f0ef
Greyma <40049913+Greyma@users.noreply.github.com>
2023-06-06 16:59:20 +0200

    Delete AddContinent.php

--- AddContinent.php
+++
@@ -1,9 +0,0 @@
-<?php 
-echo "
-<div class='contain'>
-<label for='Continent'>Continent </label> <input type='text' id='Continent' name='Continent' size='10' required>
-</div>
-<?php include 'postGet.php'?>  
-<button id='add' onclick='Sauvgarder();' name='addContinent'>Ajouter <?php Sauvgarder(); ?>  </button>
-";
-?>
